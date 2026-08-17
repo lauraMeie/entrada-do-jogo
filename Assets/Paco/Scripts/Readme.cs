35bb2b79d13a35f2db956e2f9ee9b68e2a3ab78e
lauraMeie <215929086+lauraMeie@users.noreply.github.com>
2026-08-17 19:40:30 -0300

    Check-in inicial

---
+++ Assets/Paco/Scripts/Readme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class PacoReadme : ScriptableObject
+{
+    public Texture2D icon;
+    public string title;
+    public Section[] sections;
+    public bool loadedLayout;
+
+    [Serializable]
+    public class Section
+    {
+        public string heading, text, linkText, url;
+    }
+}
